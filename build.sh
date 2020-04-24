86e25a815a5a05fcdf902bfdf53ed74c2a87de75
korostelevm <korostelevm@gmail.com>
2020-04-24 07:07:56 -0400

    update test

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 sam build -b ./build --use-container -m ./requirements.txt
 
 # Move dependencies into our layer directory
-mv ./build/ImageProcessorFunction/PIL ./src/
-mv ./build/ImageProcessorFunction/Pillow* ./src/
+mv ./build/ServiceApiFunction/PIL ./src/
+mv ./build/ServiceApiFunction/Pillow* ./src/
 
 # Remove the build directory and its contents
 rm -rf ./build
